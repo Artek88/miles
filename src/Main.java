a6e1e4f82c975f63a9315d2534c0ba78c50ae06c
Artek88 <artem.e3k@gmail.com>
2022-04-30 14:46:04 +0800

    miles

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 10000;
+        int bonus = 20;
+        int ticketBonus = ticket / bonus;
+
+        System.out.println("ticketBonus: " + ticketBonus);
+
+    }
+}
+
